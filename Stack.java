4b6a939bb949fc0312b4bb3f0d183c23d5b53af9
rgoggins <ryanngoggins@gmail.com>
2017-03-28 12:39:06 -0400

    just for sam to see what I was doing to start

--- Stack.java
+++ Stack.java
@@ -1,11 +1,43 @@
 /**
  * A Stack class
  *
- * Author:
- * Date:
+ * Author: Ryan Goggins & Sam Xifaras
+ * Date: 3/28/17
  * Course: CSC630 Data Structures and ALgorithms
  */
+ public class Stack<T> extends LinkedList<T> {
 
-public class Stack<T> {
-    private T contents;
+  private T contents;
+  private Stack<T> head;
+
+  //no args constructor
+  public Stack() {
+    contents = null;
+  }
+
+  public boolean empty() {
+    //This method tests if the Stack is empty
+    return (head == null);
+  }
+
+  public T peek() {
+    return contents;
+  }
+
+  public T pop() {
+    //add it to index 0
+
+  }
+
+  public T push(T item) {
+
+  }
+
+  public int search(Object o) {
+
+  }
+
+  public String toString() {
+
+  }
 }
