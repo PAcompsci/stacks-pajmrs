f76094383c1833c71e27464083ab66b00a6682a5
mchao409 <mchao409@gmail.com>
2017-03-28 12:51:22 -0400

    use linked list

--- Stack.java
+++ Stack.java
@@ -5,39 +5,42 @@
  * Date: 3/28/17
  * Course: CSC630 Data Structures and ALgorithms
  */
- public class Stack<T> extends LinkedList<T> {
-
-  private T contents;
-  private Stack<T> head;
-
+ 
+ public class Stack<T> {
+  private LinkedList<T> contents;
+  
   //no args constructor
   public Stack() {
     contents = null;
   }
-
+  
   public boolean empty() {
     //This method tests if the Stack is empty
-    return (head == null);
+    return contents.size() == 0;
   }
 
   public T peek() {
-    return contents;
+    return contents.get(0);
   }
 
   public T pop() {
+    contents.remove(0);
     //add it to index 0
 
   }
 
   public T push(T item) {
-
+    contents.add(0,item);
   }
 
   public int search(Object o) {
+    int index = contents.indexOf(o);
+    if(index == -1) return -1;
+    return index+1;
 
   }
 
   public String toString() {
-
+    return contents + "";
   }
 }
