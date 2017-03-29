4ff263d88a032cc1819f9d260a5aca0ff8d662ac
rgoggins <ryanngoggins@gmail.com>
2017-03-29 16:06:31 -0400

    Wrote recursive check+started branch recursion

--- MethodReport.java
+++ MethodReport.java
@@ -2,12 +2,12 @@
  * A mininmal working example of an Inspector: a class that can view the
  * stack trace of different Thread.
  *
- * Author: Jocelyn Shen, Michelle Chao, Sam Xifaras, Ryan Goggins
+ * Author: Ryan, Sam, Jocelyn, Michelle
  * Date: March 28, 2017
  * Course: CSC630: Data Structures and Algorithms
  */
- 
- 
+
+
  // NOTE: Use reflection API to call methods by name
 
 import java.util.Arrays;
@@ -15,7 +15,7 @@ import java.util.Arrays;
 public class MethodReport{
   private String methodName;
   private LinkedList<Stack<String>> timeline;
-  
+
   public MethodReport(String name) {
     methodName = name;
   }
@@ -27,14 +27,37 @@ public class MethodReport{
   /**
   * Return true if the method is recursive (branched or otherwise).
   */
-  
+
   // not done
   public boolean isRecursive() {
+    //.getAllStackTraces
+    Map<Thread,StackTraceElement[]> stacks = Thread.getAllStackTraces();
+    for (Thread newT : stacks) {
+      StackTraceElement[] newStack = stacks.get(newT);
+      String recentMethod = "";
+      for (int i = 0; i < newStack.length; i++) {
+        //this goes through the specific stacktrace element array and checks whether the previous
+        //contained the same method
+        StackTraceElement stackElement = newStack[i];
+        if (stackElement.getMethodName().equals(recentMethod)) {
+          //2 methods used in a row?
+          //does this mean that its recursive??
+          return true;
+        }
+        recentMethod = stackElement.getMethodName();
+      }
+      //get the new thread iterate through the array of stack traceelements
+      //look at each stack trace element
+      //determine whether it is recursive
+    }
     return false;
-
   }
+
   // not done
   public boolean isBranchedRecursive() {
+    //branched recursive is when the method calls another method and then goes back and returns to being recursive
+
+
     return false;
 
   }
