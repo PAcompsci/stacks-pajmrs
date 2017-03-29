012455f304a4b1220b20a69b4fcba880e23ecb2e
mchao409 <mchao409@gmail.com>
2017-03-28 23:30:45 -0400

    some changes to Inspector and MethodReport

--- Inspector.java
+++ Inspector.java
@@ -1,20 +1,66 @@
 /**
- * A mininmal working example of an Inspector: a class that can view the
- * stack trace of different Thread.
  *
  * Author: Jocelyn Shen, Michelle Chao, Sam Xifaras, Ryan Goggins
  * Date: March 28, 2017
  * Course: CSC630: Data Structures and Algorithms
  */
 
-import java.util.Arrays;
+import java.util.*;
+import java.lang.reflect.*;
 
-public class Inspector implements Runnable {
-  public ArrayList<MethodReport> InspectAll() {
+public class Inspector {
+  private Thread t;
+  private Thread inspecteeThread;
+  
+  public Inspector() {};
+  
+  
+  private ArrayList<MethodReport> InspectAll(String className) {
+    ArrayList<MethodReport> report = new ArrayList<MethodReport>();
+    try {
+        Class<?> c = Class.forName(className);
+        Object t = c.newInstance();
+        Method[] methods = c.getDeclaredMethods();
+        for (Method m: methods) {
+            System.out.println("\n" + m);
+            Parameter[] p = m.getParameters();
+            Type type = p[0].getParameterizedType();
+            // TODO: Invoke method w/ argument(s) of proper type
+            
+            
+            
+            
+            
+            report.add(new MethodReport(m.getName()));
+        }
+    } catch (Throwable e) {
+        System.err.println(e);
+    }
+    return report;
+  }
+  
+  private void run() {
+      for(int i = 4; i > 0; i--) {
+          try {
+              /* We will eventually talk about maps.  For now, just use
+               * these lines together!
+               */
+              Map<Thread,StackTraceElement[]> stacks = Thread.getAllStackTraces();
+              System.out.println(Arrays.toString(stacks.get(inspecteeThread)));
 
+              // Pause the thread for 50 milliseconds
+              Thread.sleep(50);
+          } catch (InterruptedException e) {
+              /* Sleep in Java needs to be caught, because it throws
+               * exception when it completes.
+               */
+          }
+      }
   }
 
    public static void main(String args[]) {
-
+     Inspector a = new Inspector();
+     a.InspectAll("LinkedList");
+     
     }
 }
--- MethodReport.java
+++ MethodReport.java
@@ -6,12 +6,19 @@
  * Date: March 28, 2017
  * Course: CSC630: Data Structures and Algorithms
  */
+ 
+ 
+ // NOTE: Use reflection API to call methods by name
 
 import java.util.Arrays;
 
 public class MethodReport{
   private String methodName;
   private LinkedList<Stack<String>> timeline;
+  
+  public MethodReport(String name) {
+    methodName = name;
+  }
 
   public LinkedList getTimeline() {
     return timeline;
@@ -20,11 +27,15 @@ public class MethodReport{
   /**
   * Return true if the method is recursive (branched or otherwise).
   */
+  
+  // not done
   public boolean isRecursive() {
+    return false;
 
   }
-
+  // not done
   public boolean isBranchedRecursive() {
+    return false;
 
   }
 }
