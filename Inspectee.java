b423a3f99717184c956bb068ae8d488f5f124eaf
U-JOCIE-YOGAPRO2\jocel <jocelynshenbb@gmail.com>
2017-03-30 12:30:46 -0400

    added inspectee file

---
+++ Inspectee.java
@@ -0,0 +1,37 @@
+/**
+ * A mininmal working example of a thread, set up to work with an
+ * Inspector object.
+ *
+ * Author: Nicholas Zufelt
+ * Date: March 28, 2017
+ * Course: CSC630: Data Structures and Algorithms
+ */
+
+class Inspectee implements Runnable {
+    private Thread t;
+
+    Inspectee() {
+        t = new Thread(this, "Inspectee object's thread");
+    }
+
+    public Thread getT() {return t;}
+
+    public void run() {
+        waitAWhile(50);
+    }
+
+    // This method was made to be recursive for the Inspector's perusal
+    public void waitAWhile(int n) {
+        if (n == 1) return;
+        try {
+            // Total sleep time = n * 20 milliseconds
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+        }
+        waitAWhile(n-1);
+    }
+
+    public void start() {
+        t.start();
+    }
+}
--- Inspector.java
+++ Inspector.java
@@ -1,7 +1,7 @@
 /**
  * Inspects a class for its methods. If the methods have primitive arguments,
  * the Inspector calls these methods with arguments.
- * 
+ *
  * Author: Jocelyn Shen, Michelle Chao, Sam Xifaras, Ryan Goggins
  * Date: March 28, 2017
  * Sources: Java reflect API
@@ -13,25 +13,26 @@
 import java.util.*;
 import java.lang.reflect.*;
 
-public class Inspector {
+public class Inspector implements Runnable{
   private int[] integer_options;
   private String[] string_options;
   private Thread t;
   private Thread inspecteeThread;
-  
-  public Inspector() {
+
+  public Inspector(Thread iT)
+    iT = inspecteeThread;
     integer_options = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14};
     string_options = new String[]{"dog", "cat," ,"house", "computer science"};
   }
-  
+
     /**
-    * Inspects the given class for its methods with their return values if 
+    * Inspects the given class for its methods with their return values if
     * callable, and adds each method to a string.
     * @param className the name of the class to be inspected
     * @return an ArrayList containing MethodReport objects for each method
     * in the class
     */
-    private ArrayList<MethodReport> InspectAll(String className) {
+    private ArrayList<MethodReport> InspectAll() {
       ArrayList<MethodReport> report = new ArrayList<MethodReport>();
       try {
       Class<?> c = Class.forName(className);
@@ -51,15 +52,16 @@ public class Inspector {
             else if(p[i].isInstance(String.class)) {
               new_params[i] = string_options[rand.nextInt(string_options.length)];
             }
-            else { 
+            else {
               // Unsure of how to deal with generic types
               new_params[i] = integer_options[rand.nextInt(integer_options.length)];
             }
           }
-          Object class_instance = 
+          Object class_instance =
           Class.forName(className).newInstance();
           System.out.println("  Input: " + Arrays.toString(new_params));
           System.out.println("  Returned: " + m.invoke(class_instance, new_params));
+          System.out.println("  Is recursive or not: " );
           report.add(new MethodReport(m.getName()));
         } catch(Throwable e) {
           System.out.println("Exception thrown during method execution: " + e);
@@ -71,31 +73,31 @@ public class Inspector {
    return report;
 
   }
-  
-  // not sure if we need this method or not
-  // 
-  // private void run() {
-  //     for(int i = 4; i > 0; i--) {
-  //         try {
-  //             /* We will eventually talk about maps.  For now, just use
-  //              * these lines together!
-  //              */
-  //             Map<Thread,StackTraceElement[]> stacks = Thread.getAllStackTraces();
-  //             System.out.println(Arrays.toString(stacks.get(inspecteeThread)));
-  // 
-  //             // Pause the thread for 50 milliseconds
-  //             Thread.sleep(50);
-  //         } catch (InterruptedException e) {
-  //             /* Sleep in Java needs to be caught, because it throws
-  //              * exception when it completes.
-  //              */
-  //         }
-  //     }
-  // }
+
+  public void run() {
+      for(int i = 4; i > 0; i--) {
+          try {
+              Map<Thread,StackTraceElement[]> stacks = Thread.getAllStackTraces();
+              System.out.println(Arrays.toString(stacks.get(inspecteeThread)));
+          } catch (InterruptedException e) {
+          }
+      }
+  }
+
+  public void start () {
+      /* All examples I found online have this `if (t == null)` line.
+       * I don't really see the necessity of it, but I didn't want to
+       * just delete it and have that cause y'all all kinds of bugs.
+       */
+      if (t == null) {
+          t = new Thread(this, "Inspector object's thread");
+          t.start();
+      }
+  }
 
    public static void main(String args[]) {
      Inspector a = new Inspector();
      a.InspectAll(args[0]);
-     
+
     }
 }
